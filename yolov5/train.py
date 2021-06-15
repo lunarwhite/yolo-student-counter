7c29dcca5babf117c35b3940278ffef98e786a57
lunarwhite <dwcn22@outlook.com>
2021-06-15 11:43:00 +0800

    update train.py

--- yolov5/train.py
+++ yolov5/train.py
@@ -449,12 +449,11 @@ def train(hyp, opt, device, tb_writer=None):
     torch.cuda.empty_cache()
     return results
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='yolov5s.pt', help='initial weights path')
-    parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
-    parser.add_argument('--data', type=str, default='data/coco128.yaml', help='dataset.yaml path')
+    parser.add_argument('--weights', type=str, default='yolov5/weights/yolov5s.pt', help='initial weights path')
+    parser.add_argument('--cfg', type=str, default='yolov5/models/yolov5s.yaml', help='model.yaml path')
+    parser.add_argument('--data', type=str, default='data/headset.yaml', help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default='data/hyp.scratch.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=300)
     parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs')

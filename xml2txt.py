dec4a7cf5d540260fd4afe41114926d425b3b230
lunarwhite <dwcn22@outlook.com>
2021-06-15 11:44:13 +0800

    create xml2txt.py

---
+++ xml2txt.py
@@ -0,0 +1,51 @@
+import os
+from os import listdir
+import pickle
+import copy
+from lxml.etree import Element, SubElement, tostring, ElementTree
+import xml.etree.ElementTree as ET
+
+classes = ["head"]  # 类别
+dir_xml = 'dataset/annotations'
+dir_label = 'dataset/labels'
+
+def convert(size, box):
+    dw = 1. / size[0]
+    dh = 1. / size[1]
+    x = (box[0] + box[1]) / 2.0
+    y = (box[2] + box[3]) / 2.0
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x * dw
+    w = w * dw
+    y = y * dh
+    h = h * dh
+    return (x, y, w, h)
+
+def convert_annotation(image_id):
+    in_file = open(dir_xml+'\%s.xml' % (image_id), encoding='UTF-8')
+    out_file = open(dir_label+'\%s.txt' % (image_id), 'w')  # 生成txt格式文件
+    tree = ET.parse(in_file)
+    root = tree.getroot()
+    size = root.find('size')
+    w = int(size.find('width').text)
+    h = int(size.find('height').text)
+
+    for obj in root.iter('object'):
+        cls = obj.find('name').text
+        # print(cls)
+        if cls not in classes:
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
+             float(xmlbox.find('ymax').text))
+        bb = convert((w, h), b)
+        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+
+# xml list
+img_xmls = os.listdir(dir_xml)
+for img_xml in img_xmls:
+    label_name = img_xml.split('.')[0]
+    print(label_name)
+    convert_annotation(label_name)
